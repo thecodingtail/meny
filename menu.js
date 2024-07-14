44ddfa6ac21e3f8a086e9e47ce15642f81b92d5a
albinarcool <150587304+albinarcool@users.noreply.github.com>
2024-07-14 14:01:19 +0200

    first files

---
+++ menu.js
@@ -0,0 +1,10 @@
+let scrollDownBtn = document.getElementById("our-menu-btn")
+let startersSection = document.getElementsById("starter-section")
+
+
+
+
+function scrollDownFunction() {
+    document.body.scrollIntoView({ behavior: "smooth", block: "end" })
+  }
+
